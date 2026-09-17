f09d1ca2d7d31d23ec0cb497ae16f7880ad48ade
Ayden Swain <ayswain03@gmail.com>
2026-09-17 06:16:17 -0600

    style: add comments

--- world_layoffs_etl.sql
+++ world_layoffs_etl.sql
@@ -1,9 +1,11 @@
+-- Drop to allow for easy execution
 DROP TABLE IF EXISTS
 	layoffs_stg;
 
 CREATE TABLE layoffs_stg
 LIKE layoffs_raw;
 
+-- ########### Remove duplicates ###########
 INSERT layoffs_stg
 WITH remove_duplicates AS (
 	SELECT *,
@@ -16,10 +18,9 @@ SELECT company, location, industry, total_laid_off, percentage_laid_off, `date`,
 FROM remove_duplicates
 WHERE row_num = 1;
 
+-- Test how many duplicates were removed
 SELECT 
 	(SELECT COUNT(*) FROM layoffs_raw) AS raw_count,
     (SELECT COUNT(*) FROM layoffs_stg) AS cleaned_count,
     (SELECT COUNT(*) FROM layoffs_raw) - (SELECT COUNT(*) FROM layoffs_stg) AS duplicates_removed;
-
-SELECT *
-FROM layoffs_stg;
+-- Results: 5 duplicates removed
